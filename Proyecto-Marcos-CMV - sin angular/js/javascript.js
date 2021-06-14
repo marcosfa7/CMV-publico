1070e8d031baf80db6d73ec323228583569939ff
marcosfa7 <84081254+marcosfa7@users.noreply.github.com>
2021-06-14 10:58:56 +0200

    cambios lunes

--- Proyecto-Marcos-CMV - sin angular/js/javascript.js
+++ Proyecto-Marcos-CMV - sin angular/js/javascript.js
@@ -1,29 +1,4 @@
-<!doctype html>
-<html lang="es">
-
-<head>
-  <meta charset="utf-8">
-  <title>ProyectoMarcos</title>
-  <base href="/">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
-  <!-- scripts externos de librerias que usaremos, por ejemplo Bootstrap o JQuery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-</head>
-
-<body onload="leerXML()">
-  <app-root></app-root>
-
-</body>
-<script>
-  var peliculas = [];//Array con mis elementos
+var peliculas = [];//Array con mis elementos
 
   function leerXML() {
     // lee desde GitHub.
@@ -98,6 +73,3 @@
     };
 
   }
-</script>
-
-</html>
